a4457baee8d9449006ad4f0d9e7294f80e4de022
Krishna Chaitanya Pasala <67577627+kchaitanya954@users.noreply.github.com>
2024-08-19 22:21:50 +0530

    Update notifications.py

--- project_manager/projects/utils/notifications.py
+++ project_manager/projects/utils/notifications.py
@@ -5,8 +5,8 @@ from django.conf import settings
 def send_telegram_message(message):
     bot_token = os.getenv('TELEGRAM_BOT_TOKEN')
     chat_id = os.getenv('TELEGRAM_CHAT_ID')
-    # bot_token = '6707751756:AAGeoshdxgdBbvbpADdugcxdn5e8FGlvS_U'
-    # chat_id = '1289952334'
+    # bot_token = 'enter your token'
+    # chat_id = 'enter chat id'
     api_url = f'https://api.telegram.org/bot{bot_token}/sendMessage'
     
     payload = {
